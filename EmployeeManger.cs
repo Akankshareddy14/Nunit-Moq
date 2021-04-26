30bead4f1c217eb2152c55c51d4043c9b6708c0f
Akankshareddy14 <51977210+Akankshareddy14@users.noreply.github.com>
2021-04-26 13:36:20 +0530

    Add files via upload

---
+++ EmployeeManger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands_T02_HOL_005
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class EmployeeManger
+    {
+        private readonly List<Employee> employees;
+        public int HeadCount { get; set; }
+
+        public EmployeeManger()
+        {
+            employees = new List<Employee>();
+            HeadCount = employees.Count();
+        }
+        public void AddEmployee(Employee empObj)
+        {
+            employees.Add(empObj);
+            HeadCount++;
+        }
+    }
+}
---
+++ TestAddEmp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hands_T02_HOL_005;
+using NUnit.Framework;
+namespace TestClass
+{
+    [TestFixture]
+    public class TestAddEmp
+    {
+        EmployeeManger em = new EmployeeManger();
+        [TestCase]
+        public void TestCase_AddEmployee()
+        {
+            int beforeInsert = em.HeadCount;
+            Assert.AreEqual(0, beforeInsert);
+
+            Employee model = new Employee
+            {
+                Id = 1001,
+                Name = "Kishan"
+            };
+
+
+            em.AddEmployee(model);
+
+
+            int afterInsert = em.HeadCount;
+            Assert.AreEqual(1, afterInsert);
+        }
+
+    }
+}
